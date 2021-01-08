822314be0faf365c0ab03df17522724cac7262db
Zaki Zughbi <admin@admins-MBP.home>
2021-01-07 23:22:08 -0500

    completed home page

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-
 // Variables
 let sloganImg = document.getElementById('sloganImg');
 
-// Getting Scroll Area
-// var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-
-console.log(document.body.scrollTop);
-console.log(document.documentElement.scrollTop);
-
 
 // Calling Appear & Disapear Function
-window.onscroll = function(){myFunction();};
+window.onscroll = function(){appearDisapear();};
 
 
 // Image Appear & Disapear Function
-function myFunction(){
+function appearDisapear(){
     if(document.body.scrollTop > 4500 || document.documentElement.scrollTop > 3500){
         sloganImg.style.visibility='hidden';
-
     }else{
         sloganImg.style.visibility='visible';
     }
