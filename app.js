af9d18d3852cbd84593cfe56fe5d0a5064814010
Zaki Zughbi <admin@admins-MBP.home>
2021-01-07 21:01:33 -0500

    slogan window bug fixed

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+
+// Variables
+let sloganImg = document.getElementById('sloganImg');
+
+// Getting Scroll Area
+// var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+console.log(document.body.scrollTop);
+console.log(document.documentElement.scrollTop);
+
+
+// Calling Appear & Disapear Function
+window.onscroll = function(){myFunction();};
+
+
+// Image Appear & Disapear Function
+function myFunction(){
+    if(document.body.scrollTop > 4500 || document.documentElement.scrollTop > 3500){
+        sloganImg.style.visibility='hidden';
+
+    }else{
+        sloganImg.style.visibility='visible';
+    }
+}
