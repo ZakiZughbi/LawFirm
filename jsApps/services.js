50c8c5c1b41ecd27c4f449a9769b2196fbc8ad39
Zaki Zughbi <admin@admins-MBP.home>
2021-01-12 18:42:00 -0500

    completed services page, bugs fixed

--- jsApps/services.js
+++ jsApps/services.js
@@ -1,37 +1,25 @@
-    // Variables
+// Variables
 let box = document.querySelectorAll('.service-box');
-let p = document.querySelectorAll('.service-box p');
-let h2 = document.querySelectorAll('.service-box h2');
-let container = document.querySelector('.services-container');
-    // console.log(event.target.tagName)
+let footerSpace = document.querySelector('.aboveFooterSpace');
 
-//Good
-    // box.forEach((box)=> {
-    //     box.addEventListener('click', ()=> {
-    //         box.childNodes[3].style.display='block';
-    //         box.childNodes[1].style.paddingBottom='4%';
-    //         box.style.background='#5A9FB5';
-    //         box.style.color='#F0F0ED';
-    //
-    //         for(opacity = 1; opacity < 0; opacity = opacity - 0.1){
-    //             setTimeout(()=>{box.style.opacity='opacity';}, 100);
-    //         }
-    //         box.nextSibling.nextSibling.style.width='100%'
-    //         box.nextSibling.nextSibling.style.left='0'
-    //         console.log(box.nextSibling.nextSibling)
-    //     });
-    // });
-//End
+
+// Click Services for Details
 box.forEach((box)=> {
     box.addEventListener('click', ()=> {
-        box.style.animation = 'open 3s forwards'
-        box.style.backgroundColor = '#5A9FB5'
-        box.childNodes[3].style.opacity='1'
-        box.childNodes[1].style.color='#F0F0ED'
-    })
-})
+        // Services Details Box
+        box.style.animation = 'open 3s forwards';
+        box.style.backgroundColor = '#5A9FB5';
+        // P tag
+        box.childNodes[3].style.opacity='1';
+        // H2 tag
+        box.childNodes[1].style.color='#F0F0ED';
+        // Border Animation
+        let border = box.nextSibling.nextSibling.childNodes[1];
+        setTimeout(()=>{border.style.animation = 'border 3s forwards';}, 2000);
+    });
+});
 
-// box.childNodes[3].style.display='none';
-// box.childNodes[1].style.paddingBottom='0';
-// box.style.color='#5A9FB5';
-// box.style.background='#F0F0ED';
+// Bottom of Services Container
+box[4].addEventListener('click', ()=> {
+    footerSpace.style.height = '0';
+});
